99e2e10880a0850903d92a0221c8317fc98963a1
Pluvic <victorpluvinage25@gmail.com>
2025-11-27 17:58:37 +0100

    Update Doc prime

--- cryptool/prime.py
+++ cryptool/prime.py
@@ -2,8 +2,16 @@
 
 import random
 
-def temoinMillerRabin(a, p):
-    """Perform the Miller-Rabin primality test for a given base a and odd integer p > 2."""
+def temoinMillerRabin(a: int, p: int) -> bool:
+    """Perform the Miller-Rabin primality test.
+    
+    Args:
+        a (int): The base for the test.
+        p (int): The odd integer to be tested for primality.
+        
+    Returns:
+        bool: True if p is probably prime, False if p is composite.
+    """
     q = p - 1
     k = 0
 
@@ -23,8 +31,16 @@ def temoinMillerRabin(a, p):
         k -= 1
     return False
 
-def millerRabin(p, n):
-    """Perform the Miller-Rabin primality test n times to determine if p is probably prime."""
+def millerRabin(p: int, n: int) -> bool:
+    """Perform the Miller-Rabin primality test n times to determine if p is probably prime.
+    
+    Args:
+        p (int): The integer to be tested for primality.
+        n (int): The number of iterations to perform.
+        
+    Returns:
+        bool: True if p is probably prime, False if p is composite.
+    """
 
     if p <= 1 or p == 4:
         return False
@@ -37,16 +53,30 @@ def millerRabin(p, n):
             return False
     return True
 
-def gatherPrimes(fileName):
-    """Read prime numbers from a file and return them as a list of integers."""
+def gatherPrimes(filePath: str) -> list[int]:
+    """Read prime numbers from a file and return them as a list of integers.
+    
+    Args:
+        filePath (str): The path to the file containing prime numbers.
+        
+    Returns:
+        list[int]: A list of prime numbers read from the file.
+    """
     primes = []
-    with open(fileName, 'r') as file:
+    with open(filePath, 'r') as file:
         for line in file:
             primes.append(int(line.strip()))
     return primes
 
-def testPrime(p):
-    """Test if p is prime using trial division up to the square root of p."""
+def isPrime(p: int) -> bool:
+    """Test if p is prime using trial division up to the square root of p.
+    
+    Args:
+        p (int): The integer to be tested for primality.
+
+    Returns:
+        bool: True if p is probably prime, False if p is composite.
+    """
 
     primeList = gatherPrimes("primes_100000.txt")
     for prime in primeList:
@@ -57,12 +87,19 @@ def testPrime(p):
         
     return millerRabin(p, 40)
 
-def genPrime(n):
-    # Générer un nombre aléatoire
+def genPrime(n: int) -> int:
+    """ Generate a random prime number with n bits.
+    
+    Args:
+        n (int): The number of bits for the prime number.
+        
+    Returns:
+        int: A random prime number with n bits.
+    """
     x = random.randint(1<<(n-1), (1<<(n)))
     if x % 2 == 0:
         x += 1
 
-    while (not testPrime(x)):
+    while (not isPrime(x)):
         x += 2
     return x
